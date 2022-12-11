ee2b48171fccadb96a3e998ec882a161ab8910b5
Kim Phan <kimphan@Kims-MacBook-Pro.local>
2022-12-11 15:16:49 -0800

    first commit

---
+++ aimAtFirst.py
@@ -0,0 +1,13 @@
+#aimAtFirst.py
+
+import maya.cmds as cmds
+
+selectionList = cmds.ls( orderedSelection = True)
+
+if len(selectionList) >=2:
+    
+    targetName = selectionList[0]
+    selectionList.remove(targetName)
+    
+    for objectName in selectionList:
+        cmds.aimConstraint(targetName, objectName, aimVector = [0,1,0] )
---
+++ keyRotation.py
@@ -0,0 +1,30 @@
+#keyRotation.py
+
+import maya.cmds as cmds
+
+def keyFullRotation( pObjectName, pStartTime, pEndTime, pTargetAttribute):
+    
+        cmds.cutKey(pObjectName, time=(pStartTime,pEndTime), attribute=pTargetAttribute)
+        
+        cmds.setKeyframe(pObjectName, time=pStartTime, attribute=pTargetAttribute, value=0)
+        
+        cmds.setKeyframe(pObjectName, time=pEndTime, attribute=pTargetAttribute, value=360)
+        
+        cmds.selectKey(pObjectName, time=(pStartTime,pEndTime), attribute=pTargetAttribute, keyframe=True)
+        
+        cmds.keyTangent(inTangentType='linear', outTangentType='linear')
+        
+
+selectionList = cmds.ls( selection = True , type = 'transform')
+#click on the sphere and run the codes above to make it transform
+
+if len(selectionList) >=1:
+    
+    startTime = cmds.playbackOptions (query=True, minTime=True)
+    endTime = cmds.playbackOptions (query=True, maxTime=True)
+    
+    for objectName in selectionList:
+        keyFullRotation(objectName, startTime, endTime, 'rotateY')
+
+        
+        
